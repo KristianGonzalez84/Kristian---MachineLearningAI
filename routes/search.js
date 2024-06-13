c140b2ff1ff9da70721a96769faf03c11ba81357
KristianGonzalez84 <krigon84@gmail.com>
2024-06-13 18:15:55 +0200

    search update

--- routes/search.js
+++ routes/search.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
+const Recipe = db.Recipe;
 
 router.get('/', async function(req, res, next) {
-    res.render('search')
+    res.render('search');
 });
 
+router.post('/', async function(req, res, next) {
+    const keyword = req.body.keyword;
+    try {
+        const recipes = await db.Recipe.findAll({
+            where: {
+                title: {
+                    [db.Sequelize.Op.like]: '%' + keyword + '%'
+                }
+            }
+        });
+        res.render('search', { recipes }); // Rendering search.ejs with search results
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+module.exports = router;
